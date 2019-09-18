590135346213b5f7dbdd6c807620879e9a02f85a
Cameron-Jade Mummery <cami.mummery@aexp.com>
2019-09-18 14:09:22 +0100

    reading from the csv using readalllines, incorrect syntax atm

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,7 +1,44 @@
 package training.supportbank;
 
+import javax.imageio.IIOException;
+import java.math.BigDecimal;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
+
+
+    /*class person {
+        String name;
+        BigDecimal balance;
+    }
+
+    class transfer {
+        Date dates;
+        BigDecimal initialbalance;
+        BigDecimal newbalance;
+
+    }*/
+
     public static void main(String args[]) {
+
+
+        Path transactions = Paths.get("Transactions2014.csv");
+
+        try {
+            List<String> lines = Files.readAllLines(transactions);
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        }catch (IIOException e) {
+            System.out.println(e);
+        }
+
+
+
         // Your code here!
         System.out.println("Test!");
     }
