0a31b2fd618ee6c39744975f94789e13c77290e0
Cameron-Jade Mummery <cami.mummery@aexp.com>
2019-09-19 12:43:08 +0100

    adding user to the list

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -19,25 +19,50 @@ public class Main {
     public static void main(String args[]) throws IOException {
 
         List<String> line = Files.readAllLines(Paths.get("Transactions2014.csv"), Charset.forName("windows-1252"));
+        line.remove(0);
+        ArrayList<Person> people = new ArrayList<Person>();
 
         for(int i = 0; i < line.size(); i++) {
+            System.out.println(people.size());
+            boolean userExists = false;
+            int userIndex;
+            String[] items = line.get(i).split(",");
 
+            for (int l = 0; l <= people.size() - 1; l++){
 
-
-            if(i>0){
-                String[] items = line.get(i).split(",");
-
-
-                Person each = new Person();
-                each.name = items[1];
-                each.balance = new BigDecimal(items[4]);
-                System.out.println(each.name);
-                System.out.println(each.balance);
-
+                if (people.get(l).getName().equals(items[1])) {
+                    userExists = true;
+                    userIndex = l;
+                }
 
             }
+                 if (userExists == true){
+
+
+                 }else {
+                     people.add(new Person(items[1]));
+                 }
 
         }
 
     }
 }
+
+                /*each.name = items[1];
+
+
+                each.balance = new BigDecimal(items[4]);
+
+                System.out.println(each.name);
+                System.out.println(each.balance);*/
+
+
+
+               /* Transaction eachTransaction = new Transaction();
+                eachTransaction.date = new Date(items[0]);
+                eachTransaction.toName = each.name;
+                eachTransaction.fromName = items[2];
+                eachTransaction.narrative = items[3];*/
+
+
+
--- src/main/java/training/supportbank/Person.java
+++ src/main/java/training/supportbank/Person.java
@@ -3,7 +3,20 @@ package training.supportbank;
 import java.math.BigDecimal;
 
 class Person {
-        String name;
-        BigDecimal balance;
+        private String name;
+        private BigDecimal balance;
+
+        public Person (String name){
+                this.name = name;
+        }
+        public String getName(){
+                return name;
+        }
+        public void changeBalance(BigDecimal value) {
+                balance = value;
+        }
+        public BigDecimal getBalance() {
+                return balance;
+        }
 
 }
